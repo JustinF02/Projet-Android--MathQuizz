e4d0599611741ed43a082d3f288ef5bb28634a3d
Ferdinand Justin <justin.ferdinand2002@gmail.com>
2022-03-16 14:26:59 +0100

    Nouvelle architecture

--- app/src/main/java/com/Projet/Android/main/PlayZone.java
+++ app/src/main/java/com/Projet/Android/main/PlayZone.java
@@ -102,6 +102,73 @@ public class PlayZone extends AppCompatActivity{
         return super.onCreateOptionsMenu(menu);
     }
 
+    private void genereUnTypeOperation(){
+        Random random = new Random();
+        int mainInnocente = random.nextInt(4);
+        switch (mainInnocente){
+            case 0:
+                this.typeOperation = TypeOperation.ADD;
+                break;
+            case 1:
+                this.typeOperation = TypeOperation.MULTIPLY;
+                break;
+            case 2:
+                this.typeOperation = TypeOperation.SUBSTRACT;
+                break;
+            /*default:
+                this.typeOperation = TypeOperation.DIVIDE;
+                break;*/
+        }
+    }
+    private void calculeUneOperation(){
+        try{
+            switch (typeOperation){
+                case ADD:
+                    resultatCorrect = element1 + element2;
+                    break;
+            /*case DIVIDE:
+                resultatCorrect = (double) element1/element2;
+                break;*/
+                case SUBSTRACT:
+                    resultatCorrect = element1-element2;
+                    break;
+                case MULTIPLY:
+                    resultatCorrect = element1 * element2;
+                    break;
+            }
+        }catch (Exception e){
+            finish();
+        }
+
+        /*Permet d'arrondir aux centaines d'unité :
+         * resultatCorrect *=100;
+         * int variableTransition = (int)((double)resultatCorrect);
+         * resultatCorrect = (double)variableTransition / 100;*/
+    }
+    private void genereUneOperation(){
+        Random random = new Random();
+        switch(level){
+            case EASY:
+                element1 = random.nextInt(maxiEASY + 1);
+                element2 = random.nextInt(maxiEASY + 1);
+                typeOperation = TypeOperation.ADD;
+                calculeUneOperation();
+                break;
+            case MEDIUM:
+                element1 = miniMedium + random.nextInt(maxiMedium);
+                element2 = miniMedium + random.nextInt(maxiMedium);
+                genereUnTypeOperation();
+                calculeUneOperation();
+                break;
+            case DIFFICULT:
+                element1 = miniDifficult + random.nextInt(maxiDifficult);
+                element2 = miniDifficult + random.nextInt(maxiDifficult);
+                genereUnTypeOperation();
+                calculeUneOperation();
+                break;
+        }
+    }
+
     private void appuieBouton(Button bouton) {
         if(operationValidee){
             if(nbLife <= 0){FinishGame();}
@@ -171,6 +238,31 @@ public class PlayZone extends AppCompatActivity{
         }
     }
 
+
+    private void FinishGame() {
+        updateBDD();
+        //TODO : UPDATE IN DB. VALUES NbOP & nbSuc etc etc
+
+        //TODO : UPDATE AUSSI DANS LA METHODE ouvreActivityScore().
+        finish(); }
+
+    private void affichage(){
+        txtVOperation.setText(element1 + " " + typeOperation.getSymbol() + " " + element2 + " = " + result);
+    }
+    private void efface(){
+        result = "?";
+        element1 = 0;
+        element2 = 0;
+        answer = 0;
+        resultatCorrect = 0;
+        typeOperation = null;
+        operationValidee = false;
+        txtAnswer.setText("");
+        txtAnswer.setBackgroundResource(R.color.white);
+        genereUneOperation();
+        affichage();
+    }
+
     private void updateBDD(){
         Score score = new Score();
         values = new ContentValues();
@@ -195,89 +287,6 @@ public class PlayZone extends AppCompatActivity{
                 break;
         }
         scoreService.updateInDb(values, score);
-    }
-    private void FinishGame() {
-        updateBDD();
-        //TODO : UPDATE IN DB. VALUES NbOP & nbSuc etc etc
-
-        //TODO : UPDATE AUSSI DANS LA METHODE ouvreActivityScore().
-        finish(); }
-    private void genereUnTypeOperation(){
-        Random random = new Random();
-        int mainInnocente = random.nextInt(4);
-        switch (mainInnocente){
-            case 0:
-                this.typeOperation = TypeOperation.ADD;
-                break;
-            case 1:
-                this.typeOperation = TypeOperation.MULTIPLY;
-                break;
-            case 2:
-                this.typeOperation = TypeOperation.SUBSTRACT;
-                break;
-            /*default:
-                this.typeOperation = TypeOperation.DIVIDE;
-                break;*/
-        }
-    }
-    private void calculeUneOperation(){
-        switch (typeOperation){
-            case ADD:
-                resultatCorrect = element1 + element2;
-                break;
-            /*case DIVIDE:
-                resultatCorrect = (double) element1/element2;
-                break;*/
-            case SUBSTRACT:
-                resultatCorrect = element1-element2;
-                break;
-            case MULTIPLY:
-                resultatCorrect = element1 * element2;
-                break;
-        }
-        /*Permet d'arrondir aux centaines d'unité :
-        * resultatCorrect *=100;
-        * int variableTransition = (int)((double)resultatCorrect);
-        * resultatCorrect = (double)variableTransition / 100;*/
-    }
-    private void genereUneOperation(){
-        Random random = new Random();
-        switch(level){
-            case EASY:
-                element1 = random.nextInt(maxiEASY + 1);
-                element2 = random.nextInt(maxiEASY + 1);
-                typeOperation = TypeOperation.ADD;
-                calculeUneOperation();
-                break;
-            case MEDIUM:
-                element1 = miniMedium + random.nextInt(maxiMedium);
-                element2 = miniMedium + random.nextInt(maxiMedium);
-                genereUnTypeOperation();
-                calculeUneOperation();
-                break;
-            case DIFFICULT:
-                element1 = miniDifficult + random.nextInt(maxiDifficult);
-                element2 = miniDifficult + random.nextInt(maxiDifficult);
-                genereUnTypeOperation();
-                calculeUneOperation();
-                break;
-        }
-    }
-    private void affichage(){
-        txtVOperation.setText(element1 + " " + typeOperation.getSymbol() + " " + element2 + " = " + result);
-    }
-    private void efface(){
-        result = "?";
-        element1 = 0;
-        element2 = 0;
-        answer = 0;
-        resultatCorrect = 0;
-        typeOperation = null;
-        operationValidee = false;
-        txtAnswer.setText("");
-        txtAnswer.setBackgroundResource(R.color.white);
-        genereUneOperation();
-        affichage();
     }
 
     private boolean ouvreActivityScore() {
